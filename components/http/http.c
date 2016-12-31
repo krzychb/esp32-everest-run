fe3a66a98c8dca86d721cdd44f62c1d6d0b13b55
Krzysztof <krzychb@gazeta.pl>
2016-12-31 22:55:03 +0100

    Error handling

--- components/http/http.c
+++ components/http/http.c
@@ -1,6 +1,8 @@
 /*
  http.c - HTTP request routines
 
+ File based on https://github.com/espressif/esp-idf/tree/master/examples/03_http_request
+
  This file is part of the ESP32 Everest Run project
  https://github.com/krzychb/esp32-everest-run
 
@@ -23,6 +25,7 @@
 
 #include "include/http.h"
 
+#define RECV_BUFFER_SIZE 64
 static const char* TAG = "HTTP";
 
 
@@ -41,7 +44,7 @@ void http_client_on_disconnected(http_client_data *client, http_callback http_di
 	client->http_disconnected_cb = http_disconnected_cb;
 }
 
-void http_client_request(http_client_data *client, const char *web_server, const char *request_string)
+esp_err_t http_client_request(http_client_data *client, const char *web_server, const char *request_string)
 {
     const struct addrinfo hints = {
         .ai_family = AF_INET,
@@ -50,70 +53,76 @@ void http_client_request(http_client_data *client, const char *web_server, const
     struct addrinfo *res;
     struct in_addr *addr;
     int s, r;
-    char recv_buf[64];
+    char recv_buf[RECV_BUFFER_SIZE];
 
-    client->chunk_buffer = recv_buf;
+    client->recv_buf = recv_buf;
+    client->recv_buf_size = sizeof(recv_buf);
 
-    while(1) {
-        /* Wait for the callback to set the CONNECTED_BIT in the
-           event group.
-        */
-        ESP_LOGI(TAG, "Waiting for Wi-Fi");
-        xEventGroupWaitBits(wifi_event_group, CONNECTED_BIT,
-                           false, true, portMAX_DELAY);
+	/* Wait for the callback to set the CONNECTED_BIT in the
+	   event group.
+	*/
+	ESP_LOGI(TAG, "Waiting for Wi-Fi");
+	xEventGroupWaitBits(wifi_event_group, CONNECTED_BIT,
+					   false, true, portMAX_DELAY);
 
-        int err = getaddrinfo(web_server, "80", &hints, &res);
+	int err = getaddrinfo(web_server, "80", &hints, &res);
 
-        if(err != 0 || res == NULL) {
-            ESP_LOGE(TAG, "DNS lookup failed err=%d res=%p", err, res);
-            vTaskDelay(1000 / portTICK_RATE_MS);
-            continue;
-        }
+	if(err != 0 || res == NULL) {
+		ESP_LOGE(TAG, "DNS lookup failed err=%d res=%p", err, res);
+		vTaskDelay(1000 / portTICK_RATE_MS);
+		return ESP_ERR_HTTP_DNS_LOOKUP_FAILED;
+	}
 
-        /* Code to print the resolved IP.
-           Note: inet_ntoa is non-reentrant, look at ipaddr_ntoa_r for "real" code
-         */
-        addr = &((struct sockaddr_in *)res->ai_addr)->sin_addr;
-        ESP_LOGI(TAG, "DNS lookup succeeded. IP=%s", inet_ntoa(*addr));
+	/* Code to print the resolved IP.
+	   Note: inet_ntoa is non-reentrant, look at ipaddr_ntoa_r for "real" code
+	 */
+	addr = &((struct sockaddr_in *)res->ai_addr)->sin_addr;
+	ESP_LOGI(TAG, "DNS lookup succeeded. IP=%s", inet_ntoa(*addr));
 
-        s = socket(res->ai_family, res->ai_socktype, 0);
-        if(s < 0) {
-            ESP_LOGE(TAG, "... Failed to allocate socket.");
-            freeaddrinfo(res);
-            vTaskDelay(1000 / portTICK_RATE_MS);
-            continue;
-        }
-        ESP_LOGI(TAG, "... allocated socket\r\n");
+	s = socket(res->ai_family, res->ai_socktype, 0);
+	if(s < 0) {
+		ESP_LOGE(TAG, "... Failed to allocate socket.");
+		freeaddrinfo(res);
+		vTaskDelay(1000 / portTICK_RATE_MS);
+		return ESP_ERR_HTTP_FAILED_TO_ALLOCATE_SOCKET;
+	}
+	ESP_LOGI(TAG, "... allocated socket");
 
-        if(connect(s, res->ai_addr, res->ai_addrlen) != 0) {
-            ESP_LOGE(TAG, "... socket connect failed errno=%d", errno);
-            close(s);
-            freeaddrinfo(res);
-            vTaskDelay(4000 / portTICK_RATE_MS);
-            continue;
-        }
+	if(connect(s, res->ai_addr, res->ai_addrlen) != 0) {
+		ESP_LOGE(TAG, "... socket connect failed errno=%d", errno);
+		close(s);
+		freeaddrinfo(res);
+		vTaskDelay(4000 / portTICK_RATE_MS);
+		return ESP_ERR_HTTP_SOCKET_CONNECT_FAILED;
+	}
 
-        ESP_LOGI(TAG, "... connected");
-        freeaddrinfo(res);
-        client->http_connected_cb((uint32_t*) client);
+	ESP_LOGI(TAG, "... connected");
+	freeaddrinfo(res);
+	if (client->http_connected_cb) {
+		client->http_connected_cb((uint32_t*) client);
+	}
 
-        if (write(s, request_string, strlen(request_string)) < 0) {
-            ESP_LOGE(TAG, "... socket send failed");
-            close(s);
-            vTaskDelay(4000 / portTICK_RATE_MS);
-            continue;
-        }
-        ESP_LOGI(TAG, "... socket send success");
+	if (write(s, request_string, strlen(request_string)) < 0) {
+		ESP_LOGE(TAG, "... socket send failed");
+		close(s);
+		vTaskDelay(4000 / portTICK_RATE_MS);
+		return ESP_ERR_HTTP_SOCKET_SEND_FAILED;
+	}
+	ESP_LOGI(TAG, "... socket send success");
 
-        /* Read HTTP response */
-        do {
-            bzero(recv_buf, sizeof(recv_buf));
-            r = read(s, recv_buf, sizeof(recv_buf)-1);
-            client->http_process_chunk_cb((uint32_t*) client);
-        } while(r > 0);
+	/* Read HTTP response */
+	do {
+		bzero(recv_buf, sizeof(recv_buf));
+		r = read(s, recv_buf, sizeof(recv_buf)-1);
+		if (client->http_process_chunk_cb) {
+			client->http_process_chunk_cb((uint32_t*) client);
+		}
+	} while(r > 0);
 
-        ESP_LOGI(TAG, "... done reading from socket. Last read return=%d errno=%d\r\n", r, errno);
-        close(s);
-        client->http_disconnected_cb((uint32_t*) client);
-    }
+	ESP_LOGI(TAG, "... done reading from socket. Last read return=%d errno=%d", r, errno);
+	close(s);
+	if (client->http_disconnected_cb) {
+		client->http_disconnected_cb((uint32_t*) client);
+	}
+	return ESP_OK;
 }
--- components/http/include/http.h
+++ components/http/include/http.h
@@ -16,23 +16,33 @@ extern "C" {
 #endif
 
 #include "freertos/event_groups.h"
+#include "esp_err.h"
 
 extern EventGroupHandle_t wifi_event_group;
 extern const int CONNECTED_BIT;
 
 typedef void (*http_callback)(uint32_t *args);
 
-typedef struct  {
-	char *chunk_buffer;
+typedef struct {
+	char *recv_buf;
+	int recv_buf_size;
+	char *proc_buf;
+	int proc_buf_size;
 	http_callback http_connected_cb;
 	http_callback http_process_chunk_cb;
 	http_callback http_disconnected_cb;
 } http_client_data;
 
+#define ESP_ERR_HTTP_BASE 0x40000
+#define ESP_ERR_HTTP_DNS_LOOKUP_FAILED          (ESP_ERR_HTTP_BASE + 1)
+#define ESP_ERR_HTTP_FAILED_TO_ALLOCATE_SOCKET  (ESP_ERR_HTTP_BASE + 2)
+#define ESP_ERR_HTTP_SOCKET_CONNECT_FAILED      (ESP_ERR_HTTP_BASE + 3)
+#define ESP_ERR_HTTP_SOCKET_SEND_FAILED         (ESP_ERR_HTTP_BASE + 4)
+
 void http_client_on_connected(http_client_data *client, http_callback http_connected_cb);
 void http_client_on_process_chunk(http_client_data *client, http_callback http_process_chunk_cb);
 void http_client_on_disconnected(http_client_data *client, http_callback http_disconnected_cb);
-void http_client_request(http_client_data *client, const char *web_server, const char *request_string);
+esp_err_t http_client_request(http_client_data *client, const char *web_server, const char *request_string);
 
 
 #ifdef __cplusplus
